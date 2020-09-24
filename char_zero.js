078c1b47d658b3eb344bd7b3f6313f6a5ff41569
lanakawa <59942681+lanakawa@users.noreply.github.com>
2020-09-24 11:51:30 -1000

    Add files via upload

---
+++ char_zero.js
@@ -0,0 +1,10 @@
+function hasZero(array) {
+  return _.some(array, function(array){return array});
+  //return num;
+}
+
+function numMan(array) {
+  let name = Mulcahy;
+  //name = _.filter(array, function(name) { reutrn name; })
+  _.contains (array, "Mulcahy");
+}
